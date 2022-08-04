18e4c1e02289237fa9fa93529822527210c4f628
Rizki Nasution <rizkirnasution@gmail.com>
2022-08-04 08:59:35 +0700

    nomor 1

---
+++ nomor1.js
@@ -0,0 +1,31 @@
+const biodata = {
+    name: "Rizki",
+    age: 25,
+    hobbies:["coding", "baca", "nonton"],
+    IsMarried: false,
+    schoolList: [
+        {
+            name:"PNJ",
+            yearIn: 2017,
+            yearOut: 2021,
+            major: "Teknik Informatika"
+        }
+    ],
+    skills: [
+        {
+            skillName:"Coding",
+            level: "beginner"
+        },
+        {
+            skillName:"Planning",
+            level: "advanced"
+        },
+        {
+            skillName:"Makan",
+            level:"expert"
+        }
+    ],
+    interestInCoding: true
+}
+
+console.log(biodata);
